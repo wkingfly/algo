3ad03dd58ea6431ba42b662821dfa3180f1740cf
wkingfly <wkingfly@gmail.com>
2016-05-05 17:07:02 +0800

    add 'add_two_numbers' answer

--- add_two_nums.py
+++ add_two_nums.py
@@ -8,10 +8,13 @@ Output: 7 -> 0 -> 8
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -20,3 +23,13 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        carry_num = 10
+        if l1.val and l2.val:
+            lr = ListNode(l1.val+l2.val)
+            if lr.val >= carry_num:
+                lr.val = lr.val % carry_num
+                aliquo_num = lr.val // carry_num
+        if not (l1.next and l2.next) is None:
+            l1.next.val += aliquo_num
+            lr.next = self.addTwoNumbers(l1.next, l2.next)
+        return lr
