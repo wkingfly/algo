65938e065fac3ab0f3969acaf6d86b18d6d34606
wkingfly <wkingfly@gmail.com>
2016-05-08 18:15:24 +0800

    add "reverse_vowels_of_a_string" qustion

---
+++ reverse_vowels_of_a_string.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Write a function that takes a string as input and
+reverse only the vowels of a string.
+
+Example 1:
+    Given s = "hello", return "holle".
+
+Example 2:
+    Given s = "leetcode", return "leotcede".
+..note:: vowels are five word: a e i o u
+"""
+
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
